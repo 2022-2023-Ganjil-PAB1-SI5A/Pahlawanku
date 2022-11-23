c74759e1db460fb3025506eff7963cee6479e3d6
ahmfarisi <ahmfarisi@gmail.com>
2022-11-23 16:11:29 +0700

    Update AdapterGrid.java

--- app/src/main/java/com/ahmfarisi/pahlawanku/AdapterGrid.java
+++ app/src/main/java/com/ahmfarisi/pahlawanku/AdapterGrid.java
@@ -34,13 +34,12 @@ public class AdapterGrid extends RecyclerView.Adapter<AdapterGrid.ClassViewHolde
     @Override
     public void onBindViewHolder(@NonNull ClassViewHolder holder, int position) {
         ModelPahlawan pahlawan = dataPahlawan.get(position);
-        holder.tvNama.setText(pahlawan.getNama());
-        holder.tvTentang.setText(pahlawan.getTentang());
+
         Glide
                 .with(ctx)
                 .load(pahlawan.getFoto())
                 .centerCrop()
-                .into(holder.ivFoto);
+                .into(holder.ivGrid);
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
